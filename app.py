d3d22935d65b659e8685d5647820d443614189c6
kalkeshwar <kalkeshwaryamsani20@gmail.com>
2024-03-09 00:12:06 +0530

    updated:

--- app.py
+++ app.py
@@ -7,55 +7,70 @@ from sklearn import metrics
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        session.pop('Age', None)
-        session.pop('Height', None)
-        session.pop('weight', None)
-        session.pop('duration', None)
-        session.pop('Heart_rate', None)
-        session.pop('Body_temp', None)
-        return render_template('index.html')
-    elif request.method == 'POST':
-        selected_option = request.form.get('Gender')
-        if selected_option == "Male":
-            input1 = 0.0
-        else:
-            input1 = 1.0
-        
-        input2 = float(request.form['Age'])
-        input3 = float(request.form['Height'])
-        input4 = float(request.form['weight'])
-        input5 = float(request.form['duration'])
-        input6 = float(request.form['Heart_rate'])
-        input7 = float(request.form['Body_temp'])
-        
-        session['Gender'] = input1
-        session['Age'] = input2
-        session['Height'] = input3
-        session['weight'] = input4
-        session['duration'] = input5
-        session['Heart_rate'] = input6
-        session['Body_temp'] = input7
-        
-        # loading the data from csv file to a Pandas DataFrame
-        calories = pd.read_csv('calories.csv')
-        exercise_data = pd.read_csv('exercise.csv')
-        calories_data = pd.concat([exercise_data, calories['Calories']], axis=1)
-        calories_data.replace({"Gender":{'male':0,'female':1}}, inplace=True)
-        X = calories_data.drop(columns=['User_ID','Calories'], axis=1)
-        Y = calories_data['Calories']
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
-        # loading the model
-        model = XGBRegressor(objective='reg:squarederror')
-        # training the model with X_train
-        model.fit(X_train, Y_train)
-        # Add the inputs from the HTML form to X_test
-        X_test.loc[len(X_test)] = [input1, input2,input3,input4,input5,input6,input7]
-        test_data_prediction = model.predict(X_test.iloc[-1:])  # Predict only the last row (inputs from HTML)
-        result = metrics.mean_absolute_error([Y_test.iloc[-1]], test_data_prediction)  # Calculate MAE for last row
-        return render_template('index.html', result=test_data_prediction[0])
+    session.pop('Age', None)
+    session.pop('Height', None)
+    session.pop('weight', None)
+    session.pop('duration', None)
+    session.pop('Heart_rate', None)
+    session.pop('Body_temp', None)
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+
+    selected_option = request.form.get('Gender')
+    if(selected_option == "Male"):
+        input1=0.0
+        session['Gender']=input1
+    else:  
+        input1=1.0
+        session['Gender']=input1
+    
+    input2 = float(request.form['Age'])
+    session['Age']=input2
+    
+    input3 = float(request.form['Height'])
+    session['Height']=input3
+    input4 = float(request.form['weight'])
+    session['weight']=input4
+    input5 = float(request.form['duration'])
+    session['duration']=input5
+    input6 = float(request.form['Heart_rate'])
+    session['Heart_rate']=input6
+    input7 = float(request.form['Body_temp'])
+    session['Body_temp']=input7
+    
+    # loading the data from csv file to a Pandas DataFrame
+    calories = pd.read_csv('calories.csv')
+
+    exercise_data = pd.read_csv('exercise.csv')
+
+    calories_data = pd.concat([exercise_data, calories['Calories']], axis=1)
+
+    calories_data.replace({"Gender":{'male':0,'female':1}}, inplace=True)
+
+    X = calories_data.drop(columns=['User_ID','Calories'], axis=1)
+    Y = calories_data['Calories']
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+
+    # loading the model
+    model = XGBRegressor(objective='reg:squarederror')
+
+    # training the model with X_train
+    model.fit(X_train, Y_train)
+
+    # Add the inputs from the HTML form to X_test
+    X_test.loc[len(X_test)] = [input1, input2,input3,input4,input5,input6,input7]
+
+    test_data_prediction = model.predict(X_test.iloc[-1:])  # Predict only the last row (inputs from HTML)
+
+    result = metrics.mean_absolute_error([Y_test.iloc[-1]], test_data_prediction)  # Calculate MAE for last row
+
+    return render_template('index.html', result=test_data_prediction[0])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
